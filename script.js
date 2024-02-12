93504ed96b4bb5bd3ed9a3af360129a0c97974d2
danya-trikov <dtrikov.od@gmail.com>
2024-02-12 19:56:32 +0200

    added script.js file and completed tasks in it

---
+++ script.js
@@ -0,0 +1,22 @@
+// task 1
+let str = "ami";
+console.log(`${str / 2}` + str);
+
+// task 2
+let myObject1 = {name: "Danya", nickName: "Danch1k", phoneNumber: "+359877742445", 
+mail: "dtrikov.od@gmail.com", sex: "male", hobby: "basketball or chess", booleanValue: true};
+let myObject2 = {name: "Vasya", nickName: "baraban", phoneNumber: "+359834556777", 
+mail: "uservasya@gmail.com", sex: "male", hobby: "football", booleanValue: true};
+let myObject3 = {name: "Lena", nickName: "LenaGolovaCh", phoneNumber: "+380677777777", 
+mail: "lenaGolovach777@gmail.com", sex: "female", hobby: "make up", booleanValue: false};
+
+
+let myArray = [myObject1, myObject2, myObject3];
+console.log(myArray);
+
+
+
+
+
+
+
